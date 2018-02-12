835d6ee5305a161dc676a013bc09d9ce1a4ef93c
Vince Carini <vincecarini@northwesternmutual.com>
2018-02-12 12:03:34 -0600

    basic store and 2 actions created

---
+++ src/state/actions.js
@@ -0,0 +1,18 @@
+import axios from "axios"
+
+export const REQUEST_GITHUB_USER = "REQUEST_GITHUB_USER";
+export const HANDLE_RESPONSE_FROM_GITHUB = "HANDLE_RESPONSE_FROM_GITHUB";
+
+function getGitHubUser(playerNumber, userName) {
+    return (dispatch, getState, api) => {
+        dispatch({ type: REQUEST_GITHUB_USER, payload: playerNumber })
+        const promise = axios.get(api + userName);
+
+        promise.then(({ data: user }) => {
+            dispatch({ type: HANDLE_RESPONSE_FROM_GITHUB, payload: { playerNumber: playerNumber, gitHubUser: user }})
+        }, () => { })
+        promise.catch(({ data: user }) => {
+            dispatch({ type: HANDLE_RESPONSE_FROM_GITHUB, payload: {playerNumber: playerNumber, gitHubUser: undefined}});
+        })
+    }
+}
---
+++ src/state/reducer.js
@@ -0,0 +1,38 @@
+import { REQUEST_GITHUB_USER, HANDLE_RESPONSE_FROM_GITHUB } from './actions';
+
+const initialState = {
+  gitHubPlayer1Status = "",
+  gitHubPlayer2Status = "",
+  gitHubPlayer1 = undefined,
+  gitHubPlayer2 = undefined
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case REQUEST_GITHUB_USER:
+      if (action.payload === 1)
+        return { ...state, gitHubPlayer1Status: "loading" }
+      else if (action.payload === 2)
+        return { ...state, gitHubPlayer2Status: "loading" }
+
+    case REQUEST_GITHUB_USER:
+      if (action.payload.gitHubUser) {
+        if(action.payload.playerNumber === 1)
+          return { ...state, gitHubPlayer1Status: "loaded", gitHubPlayer1: action.payload.gitHubUser }
+        else if(action.payload.playerNumber === 2)
+          return { ...state, gitHubPlayer2Status: "loaded", gitHubPlayer2: action.payload.gitHubUser }
+      }
+      else {
+        if(action.payload.playerNumber === 1)
+          return { ...state, gitHubPlayer1Status: "failed", gitHubPlayer1: undefined }
+        else if(action.payload.playerNumber === 2)
+          return { ...state, gitHubPlayer2Status: "failed", gitHubPlayer2: undefined }
+      }
+      
+      
+    default:
+      return state;
+  }
+}
+
+export default reducer;
