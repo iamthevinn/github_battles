041cc790ea34b3a0c5d6b7cd5a8dd701e809ec62
Vince Carini <vincecarini@northwesternmutual.com>
2018-02-12 13:30:41 -0600

    box goes away when a successful user entered

--- src/PlayerCard.js
+++ src/PlayerCard.js
@@ -2,25 +2,12 @@ import React, { Component } from 'react';
 import { getGitHubUser } from './state/actions';
 import { connect } from "react-redux";
 
-class PlayerCard extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      userName: ""
-    }
-    this.handleUserNameChange = this.handleUserNameChange.bind(this)
-  }
-
-  handleUserNameChange(event) {
-    this.setState({ userName: event.target.value })
-  }
+class InputCard extends Component {
 
   render() {
-    console.log(this.props.playerNumber)
-    console.log(this.props.gitHubPlayer1Status)
-
     return (
       <div className="card">
+
         <div className="cardHeader">
           <div>Player {this.props.playerNumber}</div>
         </div>
@@ -31,24 +18,29 @@ class PlayerCard extends Component {
           </div>
           </div>
           <div className="small-5 medium-5 large-5 columns">
-            <input onChange={this.handleUserNameChange} style={{ fontSize: '24px' }} type="text">
+            <input onChange={this.props.handleUserNameChange} style={{ fontSize: '24px' }} type="text">
             </input>
           </div>
           <div className="small-5 medium-5 large-5 columns">
             <div></div>
           </div>
         </div>
-        { 
-          ((this.props.playerNumber === 1 && this.props.gitHubPlayer1Status === "failed") || 
-          (this.props.playerNumber === 2 && this.props.gitHubPlayer2Status === "failed")) &&
-          (
-            <div className="githubNameNotFound">
-              GitHub username does not exist, try again.
+        {
+          ((this.props.playerNumber === 1 && this.props.gitHubPlayer1Status === "failed") ||
+            (this.props.playerNumber === 2 && this.props.gitHubPlayer2Status === "failed")) ?
+            (
+              <div className="githubNameNotFound">
+                GitHub username does not exist, try again.
           </div>
-          )
+            ) :
+            (
+              <div className="githubNameNotFound">
+                &nbsp;
+          </div>
+            )
         }
         <div className="getUserButton">
-          <button onClick={() => this.props.getGitHubUser(this.props.playerNumber, this.state.userName)} disabled={!this.state.userName.trim().length} style={{ width: '100%' }} >Get User</button>
+          <button onClick={() => this.props.getGitHubUser(this.props.playerNumber, this.props.userName)} disabled={!this.props.userName.trim().length} style={{ width: '100%' }} >Get User</button>
         </div>
       </div>
     )
@@ -72,4 +64,42 @@ const mapDispatchToProps = dispatch => {
   };
 }
 
-  export default connect(mapStateToProps, mapDispatchToProps)(PlayerCard);
+const InputCardWrapped = connect(mapStateToProps, mapDispatchToProps)(InputCard);
+
+class PlayerCard extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      userName: ""
+    }
+    this.handleUserNameChange = this.handleUserNameChange.bind(this)
+  }
+
+  handleUserNameChange(event) {
+    this.setState({ userName: event.target.value })
+  }
+
+  render() {
+
+    console.log(this.props.playerNumber)
+
+    return (
+      <div>
+        {((this.props.playerNumber === 1 && !this.props.gitHubPlayer1) || (this.props.playerNumber === 2 && !this.props.gitHubPlayer2)) && (
+          <InputCardWrapped playerNumber={this.props.playerNumber} userName={this.state.userName} handleUserNameChange={this.handleUserNameChange} />
+        )}
+      </div>
+    )
+  }
+}
+
+
+const mapStateToPlayerCardProps = state => {
+  return {
+    gitHubPlayer1: state.gitHubPlayer1,
+    gitHubPlayer2: state.gitHubPlayer2
+  };
+};
+
+
+export default connect(mapStateToPlayerCardProps)(PlayerCard)
